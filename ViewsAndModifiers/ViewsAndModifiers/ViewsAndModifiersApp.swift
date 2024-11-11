b0c2f8503e56ae2765ef43ca50562caf44be13ca
Sarah <sarahireneclark@gmail.com>
2024-11-11 11:58:13 -0600

    Initial commit of project

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Sarah Clark on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
